63ce1f102f6ca34896d62e60104c2446bb7a4750
Jacob Sipple <sipple3@gmail.com>
2019-01-07 23:35:06 -0500

    fixed issue jumping immediately to the next question instead of to the answer added pictures that correlate with the answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,14 +12,19 @@ let correct = 0;
 let incorrect = 0;
 let unanswered = 0;
 let interval;
-
 // create a timer for the questions
-
+$("#start, #restart").click(function() {
+    question1();
+    thisTimer();    
+    myTimer();
+    $("#start").hide();
+    $("#restart").hide();
+});
 function myTimer() {
 $("#time").text("Time remaining: " + timeLeft + " seconds")
 timeLeft--
 // if you don't guess in time
-if (timeLeft === 0) {
+if (timeLeft === -1) {
     $("#question, #answer1, #answer2, #answer3, #answer4, #time").empty();
     $("#question").text("Too late the correct answer was " + answer);
     unanswered++
@@ -32,13 +37,6 @@ function thisTimer() {
     timer = setInterval(myTimer, 1000)
     timeLeft = 30;
 }
-
-$("#start").click(function() {
-    thisTimer();
-    myTimer();
-    $("#start").hide();
-})
-
 
 // add the answer if correct or incorrcet answer given(stay on this for ~5sec) also increase
 // correct if answered correctly or wrong if incorrectly
@@ -73,7 +71,7 @@ function question3() {
 
 function question4() {
     answer = question4Answer;
-    $("#question").text("Which is the oath that all Knights radiant must take?");
+    $("#question").text("Which is the oath that all Knights Radiant must take?");
     $("#answer1").text("I will protect even those I hate, so long as it is right.");
     $("#answer2").text("I will take responsibility for what I have done. If I must fall, I will rise each time a better person.");
     $("#answer3").text("Life before death, strength before weakness, journey before destination.");
@@ -88,12 +86,6 @@ function question5() {
     $("#answer3").text("Vivenna");
     $("#answer4").text("Vin");
 }
-
-$("#start").click(function() {
-    question1();
-});
-
-
 // this registers your answer and log it under choice then need to compare it to answer for the question
 $("#answer1").click(function() {
     choice = document.getElementById("answer1").textContent;
@@ -107,9 +99,25 @@ $("#answer1").click(function() {
         $("#question").text(choice + " is incorrect the correct answer was " + answer);
         incorrect++
     }
-    interval = setInterval(nextQuestion, 3000)
-    nextQuestion();
+    if (answer === question1Answer) {
+        $("#image").attr("src", "./assets/images/pattern.png")
+    }
+    if (answer === question2Answer) {
+        $("#image").attr("src", "./assets/images/Rock.png")
+    }
+    if (answer === question3Answer) {
+        $("#image").attr("src", "./assets/images/shalash.png")
+    }
+    if (answer === question4Answer) {
+        $("#image").attr("src", "./assets/images/firstIdeal.jpg")
+    }
+    if (answer === question5Answer) {
+        $("#image").attr("src", "./assets/images/Vasher.jpg")
+    }
+    $("#image").show();
+    setTimeout(nextQuestion, 3000)
 })
+// making it a function isn't working either...
 
 $("#answer2").click(function() {
     choice = document.getElementById("answer2").textContent;
@@ -122,8 +130,24 @@ $("#answer2").click(function() {
         $("#question").text(choice + " is incorrect the correct answer was " + answer);
         incorrect++
     }
-    interval = setInterval(nextQuestion, 3000)
-    nextQuestion();
+    // check why this is not working can't seem to find anything wrong with it
+    if (answer === question1Answer) {
+        $("#image").attr("src", "./assets/images/pattern.png")
+    }
+    if (answer === question2Answer) {
+        $("#image").attr("src", "./assets/images/Rock.png")
+    }
+    if (answer === question3Answer) {
+        $("#image").attr("src", "./assets/images/shalash.png")
+    }
+    if (answer === question4Answer) {
+        $("#image").attr("src", "./assets/images/firstIdeal.jpg")
+    }
+    if (answer === question5Answer) {
+        $("#image").attr("src", "./assets/images/Vasher.jpg")
+    }
+    $("#image").show();
+    setTimeout(nextQuestion, 3000)
 })
 
 $("#answer3").click(function() {
@@ -137,8 +161,23 @@ $("#answer3").click(function() {
         $("#question").text(choice + " is incorrect the correct answer was " + answer);
         incorrect++
     }
-    interval = setInterval(nextQuestion, 3000)
-    nextQuestion();
+    if (answer === question1Answer) {
+        $("#image").attr("src", "./assets/images/pattern.png")
+    }
+    if (answer === question2Answer) {
+        $("#image").attr("src", "./assets/images/Rock.png")
+    }
+    if (answer === question3Answer) {
+        $("#image").attr("src", "./assets/images/shalash.png")
+    }
+    if (answer === question4Answer) {
+        $("#image").attr("src", "./assets/images/firstIdeal.jpg")
+    }
+    if (answer === question5Answer) {
+        $("#image").attr("src", "./assets/images/Vasher.jpg")
+    }
+    $("#image").show();
+    setTimeout(nextQuestion, 3000)
 })
 
 $("#answer4").click(function() {
@@ -152,9 +191,24 @@ $("#answer4").click(function() {
         $("#question").text(choice + " is incorrect the correct answer was " + answer);
         incorrect++
     }
+    if (answer === question1Answer) {
+        $("#image").attr("src", "./assets/images/pattern.png")
+    }
+    if (answer === question2Answer) {
+        $("#image").attr("src", "./assets/images/Rock.png")
+    }
+    if (answer === question3Answer) {
+        $("#image").attr("src", "./assets/images/shalash.png")
+    }
+    if (answer === question4Answer) {
+        $("#image").attr("src", "./assets/images/firstIdeal.jpg")
+    }
+    if (answer === question5Answer) {
+        $("#image").attr("src", "./assets/images/Vasher.jpg")
+    }
+    $("#image").show();
     // trying to delay by 3seconds so that the answer shows not sure why not working
-    interval = setInterval(nextQuestion, 3000)
-    nextQuestion();
+    setTimeout(nextQuestion, 3000)
 })
 
 // this delays it by 3 seconds 
@@ -173,11 +227,17 @@ function nextQuestion() {
         question5();
     }
     else if (answer === question5Answer) {
+        $("#question").text("Your score is:");
+        $("#answer1").text("Correct: " + correct);
+        $("#answer2").text("Incorrect: " + incorrect);
+        $("#answer3").text("Unanswered: " + unanswered);
+        $("#restart").show();
     }
     // need to change this if to something else
     if (answer !== question5Answer) {
         thisTimer();
         myTimer();
     }
-    clearInterval(interval)
+    $("#image").hide()
+    // clearInterval(interval)
 }
